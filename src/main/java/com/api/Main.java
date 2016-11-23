e0bc925c51c08fb3d8e4bd0e151379b142e27014
Yashwantrao Raut <yashwantrao.raut@plusgrade.com>
2016-11-23 23:18:00 +0530

    Registered NoteResource with dropwizard in main calss

--- src/main/java/com/api/Main.java
+++ src/main/java/com/api/Main.java
@@ -1,17 +1,18 @@
 package com.api;
 
 import com.api.config.ApiConfig;
+import com.api.resource.NoteResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 
 public class Main extends Application<ApiConfig> {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        new Main().run(args);
     }
 
     @Override
     public void run(ApiConfig configuration, Environment environment) throws Exception {
-
+        environment.jersey().register(new NoteResource());
     }
 }
