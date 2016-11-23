a5bf4cb0282b2d5f0fbc577df55ad260cfad8ac3
Yashwantrao Raut <yashwantrao.raut@plusgrade.com>
2016-11-24 00:00:59 +0530

    done dropwizard -hibernate configuration

--- src/main/java/com/api/Main.java
+++ src/main/java/com/api/Main.java
@@ -1,11 +1,24 @@
 package com.api;
 
 import com.api.config.ApiConfig;
+import com.api.domain.Note;
+import com.api.domain.User;
 import com.api.resource.NoteResource;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class Main extends Application<ApiConfig> {
+
+    private final HibernateBundle<ApiConfig> hibernate = new HibernateBundle<ApiConfig>(User.class, Note.class) {
+        @Override
+        public DataSourceFactory getDataSourceFactory(ApiConfig configuration) {
+            return configuration.getDatabase();
+        }
+    };
+
 
     public static void main(String[] args) throws Exception {
         new Main().run(args);
@@ -15,4 +28,11 @@ public class Main extends Application<ApiConfig> {
     public void run(ApiConfig configuration, Environment environment) throws Exception {
         environment.jersey().register(new NoteResource());
     }
+
+
+    @Override
+    public void initialize(Bootstrap<ApiConfig> bootstrap) {
+        bootstrap.addBundle(hibernate);
+    }
+
 }
--- src/main/java/com/api/config/ApiConfig.java
+++ src/main/java/com/api/config/ApiConfig.java
@@ -1,7 +1,26 @@
 package com.api.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class ApiConfig extends Configuration {
+
+    @JsonProperty("database")
+    public DataSourceFactory getDatabase() {
+        return database;
+    }
+
+    @JsonProperty("database")
+    public void setDatabase(DataSourceFactory database) {
+        this.database = database;
+    }
+
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
 
 }
