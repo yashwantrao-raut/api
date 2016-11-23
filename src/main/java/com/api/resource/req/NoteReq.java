5a3b14432fea6283f744ab9a20391d83a2780927
Yashwantrao Raut <yashwantrao.raut@plusgrade.com>
2016-11-23 23:04:16 +0530

    Created NoteReq to take input note details from endpoint

---
+++ src/main/java/com/api/resource/req/NoteReq.java
@@ -0,0 +1,22 @@
+package com.api.resource.req;
+public class NoteReq {
+
+    private String title;
+    private String note;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
