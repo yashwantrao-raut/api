062dd86b60898b7e0af6e6e3fea6124d87231f51
Yashwantrao Raut <yashwantrao.raut@plusgrade.com>
2016-11-24 01:19:09 +0530

    Added basic auth configuration

--- src/main/java/com/api/Main.java
+++ src/main/java/com/api/Main.java
@@ -1,10 +1,17 @@
 package com.api;
 
+import com.api.auth.BasicAuthenticator;
+import com.api.auth.UserPrincipal;
 import com.api.config.ApiConfig;
+import com.api.converter.NoteToAndFromDomainConverter;
+import com.api.dao.NoteDao;
+import com.api.dao.UserDao;
 import com.api.domain.Note;
 import com.api.domain.User;
 import com.api.resource.NoteResource;
 import io.dropwizard.Application;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
@@ -26,7 +33,14 @@ public class Main extends Application<ApiConfig> {
 
     @Override
     public void run(ApiConfig configuration, Environment environment) throws Exception {
-        environment.jersey().register(new NoteResource());
+        UserDao userDao = new UserDao(hibernate.getSessionFactory());
+        AuthDynamicFeature authDynamicFeature = new AuthDynamicFeature(
+                new BasicCredentialAuthFilter.Builder<UserPrincipal>()
+                        .setAuthenticator(new BasicAuthenticator(userDao))
+                        .setRealm("SUPER SECRET STUFF")
+                        .buildAuthFilter());
+        environment.jersey().register(authDynamicFeature);
+        environment.jersey().register(new NoteResource(new NoteToAndFromDomainConverter(),new NoteDao(hibernate.getSessionFactory())));
     }
 
 
