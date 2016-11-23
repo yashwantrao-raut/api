9188e95d3e6df4f30c6cac6644ece02e086a9c26
Yashwantrao Raut <yashwantrao.raut@plusgrade.com>
2016-11-23 23:13:16 +0530

    created post method in NoteResource to post note

--- src/main/java/com/api/resource/NoteResource.java
+++ src/main/java/com/api/resource/NoteResource.java
@@ -1,5 +1,18 @@
 package com.api.resource;
 
+import com.api.resource.req.NoteReq;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/users/{userID}/notes")
 public class NoteResource {
+
+    @POST
+    public Response post(@PathParam("userID") Long userId, NoteReq noteReq){
+        return Response.ok().build();
+    }
 
 }
